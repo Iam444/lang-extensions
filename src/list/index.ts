44ae13cd8223291676f388f8d9a25cb87d4052ac
Iam444 <iam444@mail.ru>
2026-09-01 16:26:37 +0300

    Refactored 'package.json' for better exports

---
+++ src/list/index.ts
@@ -0,0 +1,5 @@
+export { Order } from './order.js';
+export { List } from './list.js';
+export { OrderedList } from './ordered-list.js';
+export { Collection } from './collection.js';
+export { Series } from './series.js';
